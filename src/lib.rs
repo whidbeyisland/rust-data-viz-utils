ba68e3a7ddd914aff9c1dcb8188845d09900c11d
John Davis <64516850+whidbeyisland@users.noreply.github.com>
2022-11-29 17:28:44 -0800

    Trying this

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+use csv::Reader;
+use egui::Grid;
+
 pub fn add_one(x: i32) -> i32 {
     x + 1
+}
+
+pub struct GridFromCSV {
+    pub grid: Grid,
+    pub path: String,
+}
+impl GridFromCSV {
+    fn new(path: String) -> Self {
+        GridFromCSV { grid: Grid::new("some_unique_id"), path: path }
+    }
 }
